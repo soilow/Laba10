582092ef5714cc581e667bec1543b4601bd31fe3
Dmitry Zmerlov <zmerzlovd@gmail.com>
2024-04-26 14:06:18 +0300

    Initial Commit

--- lib/Scheduler.h
+++ lib/Scheduler.h
@@ -7,31 +7,31 @@ class TTaskScheduler {
 public:
     template <typename Expression, typename T>
     [[nodiscard]] NodePointer Add(Expression&& expression, const T& argument1, const T& argument2) {
-	    NodePointer new_node = CreateNode(std::move(expression), argument1, argument2);
-	    
-	    graph.InsertToNodeList(new_node);
-	    
-	    return new_node;
-	}
-	
-	template <typename Expression, typename T>
-	[[nodiscard]] NodePointer Add(Expression&& expression, const T& argument1, const T* argument2) {
-		NodePointer new_node = CreateNode(std::move(expression), argument1, argument2);
-	    
-	    graph.InsertToNodeList(new_node);
-	    
-	    return new_node;
-	}
-    
+        NodePointer new_node = CreateNode(std::move(expression), argument1, argument2);
+
+        graph.InsertToNodeList(new_node);
+
+        return new_node;
+    }
+
+    template <typename Expression, typename T>
+    [[nodiscard]] NodePointer Add(Expression&& expression, const T& argument1, const T* argument2) {
+        NodePointer new_node = CreateNode(std::move(expression), argument1, argument2);
+
+        graph.InsertToNodeList(new_node);
+
+        return new_node;
+    }
+
     template <typename Expression, typename T>
     [[nodiscard]] NodePointer Add(Expression&& expression, const T* argument1, const T* argument2) {
         NodePointer new_node = CreateNode(std::move(expression), argument1, argument2);
-        
+
         graph.InsertToNodeList(new_node);
-        
+
         return new_node;
     }
-    
+
     template <typename Expression, typename T>
     [[nodiscard]] NodePointer Add(Expression&& expression, const T& argument1) {
         NodePointer new_node = CreateNode(std::move(expression), argument1);
@@ -40,7 +40,7 @@ public:
 
         return new_node;
     }
-    
+
     template <typename Expression, typename T>
     [[nodiscard]] NodePointer Add(Expression&& expression, const T* argument1) {
         NodePointer new_node = CreateNode(std::move(expression), argument1);
@@ -50,22 +50,22 @@ public:
         return new_node;
     }
 
-	template <typename T>
-	[[nodiscard]] T GetResult(NodePointer& node) const {
-	    if (node->GetIsResultCalculated() == false) {
-	        ExecuteAll();
-	    }
-	    
-	    return *static_cast<const T*>(node->GetResult());
-	}
-    
+    template <typename T>
+    [[nodiscard]] T GetResult(NodePointer& node) const {
+        if (node->GetIsResultCalculated() == false) {
+            ExecuteAll();
+        }
+
+        return *static_cast<const T*>(node->GetResult());
+    }
+
     template <typename T>
     [[nodiscard]] inline const T* GetFutureResult(const NodePointer& node) {
         graph.InsertToAdjacencyList(node->GetNodeId(), graph.nodes_quantity);
-        
+
         return static_cast<const T*>(node->GetResult());
     }
-    
+
     void ExecuteAll() const;
 
 private:
